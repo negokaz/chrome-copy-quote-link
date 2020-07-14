b774d1dd792a78497c9474651562ebd348093fe3
Negoro Kazuki <negokaz@gmail.com>
2020-07-14 16:25:59 +0900

    ✨feat: add validation for name and scope

--- app/scripts/options/template.tsx
+++ app/scripts/options/template.tsx
@@ -5,6 +5,7 @@ import { Input, Form, Radio, Button, Icon, Card, Transition, Popup } from 'seman
 import { Template as TemplateState } from '../template';
 
 import './template.css';
+import { Menus } from 'webextension-polyfill-ts';
 
 export interface TemplateProps {
     state: TemplateState
@@ -22,6 +23,7 @@ export const Template = forwardRef<TemplateRef, TemplateProps>((props, ref) => {
 
     const [state, setState] = useState(props.state);
     const [showRemove, setShowRemove] = useState(false);
+    const [templateWasChanged, setTemplateWasChanged] = useState(false);
     const [templateError, setTemplateError] = useState<string>(null);
 
     useImperativeHandle(ref, () => ({
@@ -42,17 +44,14 @@ export const Template = forwardRef<TemplateRef, TemplateProps>((props, ref) => {
 
     const remove = useCallback(() => props.onRemove(state), []);
 
-    const handleScopeChange = useCallback((e, { value }) => setState(current => current.withEnableContexts(value)), []);
     const handleNameChange  = useCallback((e, { value }) => setState(current => current.withName(value)), []);
+    const handleScopeChange = useCallback((e, { value }) => setState(current => current.withEnableContexts(value)), []);
 
     const tryUpdateTemplate = (currentState: TemplateState, template: string) => {
+        setTemplateWasChanged(true);
         const newState = currentState.withTemplate(template);
         const result = newState.validate();
-        if (result instanceof Error) {
-            setTemplateError(result.message);
-        } else {
-            setTemplateError(null);
-        }
+        setTemplateError(result instanceof Error ? result.message : null);
         return newState;
     };
 
@@ -61,31 +60,37 @@ export const Template = forwardRef<TemplateRef, TemplateProps>((props, ref) => {
     return (
         <Card id={`template-${props.state.id}}`} fluid onMouseEnter={() => setShowRemove(true)} onMouseLeave={() => setShowRemove(false)}>
             <Card.Content>
+                <Transition visible={showRemove} animation='fade' duration={200}>
+                    <Button animated='fade' size='tiny' onClick={remove} floated='right'>
+                        <Button.Content visible><Icon name='trash' /></Button.Content>
+                        <Button.Content hidden>Remove</Button.Content>
+                    </Button>
+                </Transition>
                 <div className='element-block'>
-                    <Input size='small' placeholder='template name' ref={nameInputRef} value={state.name} onChange={handleNameChange} />
-                    <span className='element-inline'>context:</span>
-                    <Radio
-                        className='element-inline'
-                        label='page'
-                        name={scopeInputName}
-                        value='page'
-                        checked={state.enableContexts.some(c => c === 'page')}
-                        onChange={handleScopeChange}
-                    />
-                    <Radio
-                        className='element-inline'
-                        label='selection'
-                        name={scopeInputName}
-                        value='selection'
-                        checked={state.enableContexts.some(c => c === 'selection')}
-                        onChange={handleScopeChange}
-                    />
-                    <Transition visible={showRemove} animation='fade' duration={200}>
-                        <Button icon labelPosition='left' size='tiny' onClick={remove} floated='right'>
-                            <Icon name='trash' />
-                            Remove
-                        </Button>
-                    </Transition>
+                    <Form.Group inline>
+                        <Form.Field error={templateWasChanged && state.name.length === 0}>
+                            <Input size='small' placeholder='template name' ref={nameInputRef} value={state.name} onChange={handleNameChange} />
+                        </Form.Field>
+                        <label className='element-inline'>for:</label>
+                        <Form.Radio
+                            className='element-inline'
+                            label='page'
+                            name={scopeInputName}
+                            value='page'
+                            checked={state.enableContexts.some(c => c === 'page')}
+                            onChange={handleScopeChange}
+                            error={templateWasChanged && state.enableContexts.length === 0}
+                        />
+                        <Form.Radio
+                            className='element-inline'
+                            label='selection'
+                            name={scopeInputName}
+                            value='selection'
+                            checked={state.enableContexts.some(c => c === 'selection')}
+                            onChange={handleScopeChange}
+                            error={templateWasChanged && state.enableContexts.length === 0 ? 'please select' : null}
+                        />
+                    </Form.Group>
                 </div>
                 <div>
                     <Popup
